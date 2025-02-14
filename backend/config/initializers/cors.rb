a37d91b2ebacdd454e12e50669ec0ea420b4c823
domenicopasquale <domenicopasq@gmail.com>
2025-02-14 11:50:15 +0100

    Fix JSON validation error

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -8,12 +8,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Authorization', 'access-token', 'expiry', 'token-type', 'uid', 'client'],
-      max_age: 0,
+      expose: ['Authorization'],
       credentials: false
   end
 end
