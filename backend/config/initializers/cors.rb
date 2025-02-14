269fc348597e494de901738ff4d4747a70164657
domenicopasquale <domenicopasq@gmail.com>
2025-02-14 11:45:25 +0100

    Allow origins CORS

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,12 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['FRONTEND_URL'] || 'http://localhost:5173'
+    origins '*'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Authorization'],
+      expose: ['Authorization', 'access-token', 'expiry', 'token-type', 'uid', 'client'],
+      max_age: 0,
       credentials: false
   end
 end
