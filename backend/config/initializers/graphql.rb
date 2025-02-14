2d8a0d2e7c30c986d7e2a5d7053b17a727ebcb11
domenicopasquale <domenicopasq@gmail.com>
2025-02-14 12:00:09 +0100

    Fix midlleware GraphQL error

--- backend/config/initializers/graphql.rb
+++ backend/config/initializers/graphql.rb
@@ -1,9 +1,4 @@
 Rails.application.config.to_prepare do
-  # Assicuriamoci che GraphQL sia configurato correttamente in produzione
-  BackendSchema.middleware << GraphQL::Schema::TimeoutMiddleware.new(max_seconds: 10) do |err, query|
-    Rails.logger.error("GraphQL timeout: #{err.message}")
-  end
-
   if Rails.env.production?
     BackendSchema.rescue_from(StandardError) do |err, obj, args, ctx, field|
       Rails.logger.error("GraphQL error: #{err.message}")
